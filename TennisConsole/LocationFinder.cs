2ff1e724feeaa4288f401ca37e5bde59bba21118
gbahns <greg@bahns.com>
2016-01-23 10:37:11 -0500

    Added LocationFinder and StructuredMatchParser.

---
+++ TennisConsole/LocationFinder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TennisData;
+using TennisModel;
+
+namespace TennisConsole
+{
+	public static class LocationFinder
+	{
+		static TennisDb db = new TennisDb();
+
+		public static Location[] FindLocations(string name)
+		{
+			var locations = from location in db.Locations where name == location.Name select location;
+			return locations.ToArray();
+		}
+
+		public static Location[] FindLocationsContaining(string name)
+		{
+			var locations = from location in db.Locations where location.Name.Contains(name) select location;
+			return locations.ToArray();
+		}
+
+		public static Location FindLocation(string name)
+		{
+			var location = FindLocations(name);
+			if (location.Length == 0)
+				location = FindLocationsContaining(name);
+				//throw new ApplicationException("No matching locations found for '" + name + "'.");
+			if (location.Length > 1)
+				throw new ApplicationException("Multiple matching locations found for '" + name + "'.");
+			return location.First();
+		}
+
+		public static Location GetLocationById(int locationId)
+		{
+			return db.Locations.Find(locationId);
+		}
+	}
+}
---
+++ TennisLogic/StructuredMatchParser.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TennisModel;
+
+namespace TennisLogic
+{
+	public static class StructuredMatchParser
+	{
+
+
+		public static void setResult(PlayerMatch match, string token)
+		{
+			if (match.Result != "")
+				throw new FormatException("Match result is specified twice.");
+			match.Result = token.Substring(0, 1).ToUpper();
+		}
+
+		public static void setScore(PlayerMatch match, string token)
+		{
+			token = token.ToLower();
+			if (token.Contains("default"))
+			{
+				match.Defaulted = true;
+				token = token.Replace("default", "");
+			}
+			List<Set> sets = BaseMatchParser.parseMatchScore(token);
+			match.Score.Sets.AddRange(sets);
+		}
+
+		public static dynamic parseRow(this string matchString, string[] columns)
+		{
+			var defaults = new Dictionary<string, string>()
+			{
+				{"Event", "Open Play"},
+				{"Location", "Courtyard"},
+				{"Club", "Courtyard"},
+				{"Time", "6am"},
+				{"Comments", ""},
+			};
+
+			var delimiter = matchString.Contains("\t") ? "\t" : matchString.Contains(",") ? "," : " ";
+			var array = matchString.Split(delimiter.ToCharArray(), StringSplitOptions.None);
+			//var values = new Dictionary<string, string>();
+			dynamic expando = new ExpandoObject();
+			var values = (IDictionary<string, object>)expando;
+
+			for (int i = 0; i < array.Length; i++)
+			{
+				var key = columns[i];
+				var value = array[i];
+				if (value == "")
+					value = defaults[key];
+				values.Add(key, value);
+			}
+
+			return values;
+		}
+	}
+}
